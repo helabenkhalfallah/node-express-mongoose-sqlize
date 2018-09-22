d5666c1bdb16b7c02a93beeacff852f7c2959b01
helabenkhalfallah <hela@upperskills.com>
2018-09-22 16:45:49 +0100

    [node-express-mongoose-sqlize] message provider

--- messages/MesssageProvider.js
+++ messages/MesssageProvider.js
@@ -5,7 +5,7 @@ import { find } from 'lodash'
 
 // get message by key
 const messageByKey = (key) => {
-  const language = 'en'
+  const language = process.env.DEFAULT_LANGUAGE
   if (key) {
     const message = find(Messages.DATA, { 'key': key, 'language': language })
     return message ? message.value : ''
