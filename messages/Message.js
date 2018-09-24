53d909322f709684085dcde384d64940e28d56ea
helabenkhalfallah <hela@upperskills.com>
2018-09-24 09:03:41 +0100

    [node-express-mongoose-sqlize] message provider

--- messages/Message.js
+++ messages/Message.js
@@ -1,10 +1,11 @@
 // message class
 class Message {
   // constructor
-  constructor(key, value, language) {
+  constructor(key, value, language, status) {
     this._key = key
     this._value = value
     this._language = language
+    this._status = status
   }
 
   // getters & setters
@@ -26,13 +27,20 @@ class Message {
   set language(language) {
     this._language = language
   }
+  get status() {
+    return this._status
+  }
+  set status(status) {
+    this._status = status
+  }
 
   // to string
   toString() {
     return `(
       key : ${this._key}, 
       value : ${this._value} , 
-      language : ${this._language}
+      language : ${this._language},
+      status : ${this._status}
     )`
   }
 }
--- messages/Messages.js
+++ messages/Messages.js
@@ -2,13 +2,18 @@
 
 // all language keys
 const KEYS = {
-  USER_ALREADY_EXIST: 'USER_ALREADY_EXIST',
   VERIFY_REQUIRED_INFORMATION: 'VERIFY_REQUIRED_INFORMATION',
-  USER_NOT_EXIST: 'USER_NOT_EXIST',
   WRONG_PASSWORD: 'WRONG_PASSWORD',
   WRONG_SESSION: 'WRONG_SESSION',
+  USER_NOT_EXIST: 'USER_NOT_EXIST',
+  USER_ALREADY_EXIST: 'USER_ALREADY_EXIST',
   USER_ID_NOT_FOUND: 'USER_ID_NOT_FOUND',
   USER_EMAIL_NOT_FOUND: 'USER_EMAIL_NOT_FOUND',
+  USER_ADD_ERROR: 'USER_ADD_ERROR',
+  USER_UPDATE_ERROR: 'USER_UPDATE_ERROR',
+  USER_DELETE_ERROR: 'USER_DELETE_ERROR',
+  USER_LIST_ERROR: 'USER_LIST_ERROR',
+  USER_LIST_DELETE_SUCCESS: 'USER_LIST_DELETE_SUCCESS',
 }
 
 // all messages
@@ -16,40 +21,76 @@ const DATA = [
   {
     key: KEYS.USER_ALREADY_EXIST,
     value: 'User already exist.',
-    language: 'en'
+    language: 'en',
+    status: 401
   },
   {
     key: KEYS.VERIFY_REQUIRED_INFORMATION,
     value: 'Please verify required information.',
-    language: 'en'
+    language: 'en',
+    status: 401
   },
   {
     key: KEYS.USER_NOT_EXIST,
     value: 'User not exist.',
-    language: 'en'
+    language: 'en',
+    status: 401
   },
   {
     key: KEYS.WRONG_PASSWORD,
     value: 'Authentication failed. Wrong password.',
-    language: 'en'
+    language: 'en',
+    status: 401
   },
   {
     key: KEYS.WRONG_SESSION,
     value: 'An error was occured, please logout and authenticate again !',
-    language: 'en'
+    language: 'en',
+    status: 401
   },
   {
     key: KEYS.USER_ID_NOT_FOUND,
     value: 'Cannot find user with this id !',
-    language: 'en'
+    language: 'en',
+    status: 401
   },
   {
     key: KEYS.USER_EMAIL_NOT_FOUND,
     value: 'Cannot find user with this email !',
-    language: 'en'
+    language: 'en',
+    status: 401
   },
+  {
+    key: KEYS.USER_ADD_ERROR,
+    value: 'An error occured when inserting user!',
+    language: 'en',
+    status: 401
+  },
+  {
+    key: KEYS.USER_UPDATE_ERROR,
+    value: 'An error occured when updating user!',
+    language: 'en',
+    status: 401
+  },
+  {
+    key: KEYS.USER_DELETE_ERROR,
+    value: 'An error occured when deleting user!',
+    language: 'en',
+    status: 401
+  },
+  {
+    key: KEYS.USER_LIST_ERROR,
+    value: 'An error occured when getting user list !',
+    language: 'en',
+    status: 401
+  },
+  {
+    key: KEYS.USER_LIST_DELETE_SUCCESS,
+    value: 'User list deleted with success !',
+    language: 'en',
+    status: 200
+  }
 ]
-
 
 const Messages = {
   KEYS,
